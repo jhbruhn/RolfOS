fcbed3ef84e4631a5eae8c45b838b4f824b39adb
Jan-Henrik Bruhn <jh.bruhn@me.com>
2013-05-09 21:19:10 +0200

    changed docs

--- generateDocs.sh
+++ generateDocs.sh
@@ -8,3 +8,10 @@ tools/asm4doxy.pl -ud $files
 doxygen Doxyfile
 
 rm *.c
+
+git checkout gh-pages
+cp -R doc/html/* ./
+git add . -A
+git commit -m "changes to docs"
+git push origin gh-pages
+git checkout master
