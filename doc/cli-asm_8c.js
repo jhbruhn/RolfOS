18effa9385d4c08471908535080cfbb0af24a795
Jan-Henrik Bruhn <jh.bruhn@me.com>
2013-05-09 21:25:45 +0200

    removed failed stuff

--- doc/cli-asm_8c.js
+++
@@ -1,4 +0,0 @@
-var cli_asm_8c =
-[
-    [ "os_cli_input_string", "cli-asm_8c.html#a5adca1a841cabb9024eb8ffca8a7877b", null ]
-];
--- doc/keyboard-asm_8c.js
+++
@@ -1,4 +0,0 @@
-var keyboard_asm_8c =
-[
-    [ "os_keys_check_pressed", "keyboard-asm_8c.html#a8c97a171d1f8636f0c4a2e6fab0c56a8", null ]
-];
